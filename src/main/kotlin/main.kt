cba1ae46971fb0047a0624fd86067a5d920adb51
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-12-04 01:11:05 +0200

    appending json text

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,9 @@ import java.io.File
 import java.io.BufferedReader
 
 fun main(args: Array<String>) {
-    val bufferedReader: BufferedReader = File("example.txt").bufferedReader()
+    val bufferedReader:
+            BufferedReader = File("example.txt")
+        .bufferedReader()
     val inputString = bufferedReader.use { it.readText() }
     println(inputString)
 
