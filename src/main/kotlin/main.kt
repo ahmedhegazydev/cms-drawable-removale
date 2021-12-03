e0b303dec9ea779bdd524947f564834f5cb72da0
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-12-04 01:08:05 +0200

    reading text from file

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+import java.io.File
+import java.io.BufferedReader
+
+fun main(args: Array<String>) {
+    val bufferedReader: BufferedReader = File("example.txt").bufferedReader()
+    val inputString = bufferedReader.use { it.readText() }
+    println(inputString)
+
+
+}
