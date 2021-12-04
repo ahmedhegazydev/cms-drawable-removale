0fbc834a4f920b4e8b9a28584b95e236ae7300b5
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-12-04 03:23:53 +0200

    finishing adding ids

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,59 @@
-import java.io.File
 import java.io.BufferedReader
+import java.io.File
 
 fun main(args: Array<String>) {
     val bufferedReader:
-            BufferedReader = File("example.txt")
+            BufferedReader = File("./src/main/resources/android_home_json.json")
         .bufferedReader()
     val inputString = bufferedReader.use { it.readText() }
-    println(inputString)
+//    println(inputString)
+
+    val strs = inputString.split("\"icon\":").toTypedArray()
+//    println(strs.size)
+//    strs.forEach {
+//        println(it)
+//        println("====================")
+//    }
+
+    val values = mutableListOf<String>()
+    strs.forEach {
+        var res = ""
+        var counter = 0
+        var breakK = true
+        run loop@{
+            it.forEachIndexed { index, c ->
+                if (c == '"' && counter == 0) {
+                    res += it[index - 1].toString()
+                    counter++
+                } else {
+                    if (c == '"' && counter != 0) {
+                        return@loop
+                    } else
+                        res += it[index].toString()
+                }
+            }
+
+        }
+        values.add("ic_${res.trim()}")
+    }
+
+    values.removeFirst()
+
+//    println(values)
+//    values.forEach {
+//        println(it)
+//        println("====================")
+//    }
+
+    val start = "val list = mutableListOf<Int>("
+    var body = ""
+    values.forEach {
+        body += "R.id.$it, \n"
+    }
+    val end = ")"
+    val finalRes = start.plus(body).plus(end)
+    println(finalRes)
 
 
 }
+
